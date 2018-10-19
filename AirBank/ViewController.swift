6739b2b733446b524fef9faa5834d4b1c8c961ef
Tom Kraina <tomas.kraina@gmail.com>
2018-10-19 12:11:30 +0200

    Initial Commit

---
+++ AirBank/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AirBank
+//
+//  Created by Tom Kraina on 19/10/2018.
+//  Copyright © 2018 Tom Kraina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
